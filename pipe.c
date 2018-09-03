b25809f8ef285b21ae378c83ec5cbb8888bebd97
Dan F <40602274+AvCrow@users.noreply.github.com>
2018-09-02 23:03:53 -0400

    Add files via upload

--- pipe.c
+++ pipe.c
@@ -2,17 +2,18 @@
 #include<stdlib.h>
 #include<errno.h>
 #include<unistd.h>
+#include<cstring>
 int main()
 {
 	int fd[2];
 	char buffer[30];
-	pipe(fd);
+	pipe(fd); //descriptor de archivo
 	if(!fork())
 	{
 		printf("HIJO: escribiendo en el PIPE\n");
-		buffer[0]='h';
-		buffer[1]='o';
-		buffer[2]='l';
+		buffer[0]='h'; //read
+		buffer[1]='o'; //write
+		buffer[2]='l'; 
 		buffer[3]='a';
 		buffer[4]='.';
 		write(fd[1], buffer, 5);
@@ -24,6 +25,12 @@ int main()
 		printf("PADRE: leyendo desde pipe \n");
 		read(fd[0], buffer, 5);
 		printf("PADRE: he leido \%s \n", buffer);
-		
+buffer[5]='m';
+	buffer[6]='u';
+buffer[7]='n';
+buffer[8]='d';
+	buffer[9]='o';
+write(fd[1], buffer, 10);
+
 	}
 }
